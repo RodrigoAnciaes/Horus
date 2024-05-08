2eb427169573c0f51222e02c9c60daeef708b1f1
RodrigoAnciaes <81170574+RodrigoAnciaes@users.noreply.github.com>
2024-05-08 11:52:06 -0300

    Cookies and local storage.

--- horus.js
+++ horus.js
@@ -13,6 +13,8 @@ Horus.prototype.addEventListeners = function() { // Add event listeners
     var self = this;
     window.addEventListener('load', function() {
         self.detectThirdPartyConnections();
+        self.detectCookies();
+        self.detectLocalStorage();
     });
     }
 
@@ -25,7 +27,7 @@ Horus.prototype.detectThirdPartyConnections = function() { // Detect third party
     });
     var allConnectionsArray = [].concat.apply([], allConnections);
     var uniqueConnections = this.getUniqueConnections(allConnectionsArray);
-    this.showConnections(uniqueConnections);
+    //this.showConnections(uniqueConnections);
     }
 
 Horus.prototype.getConnections = function(element) { // Get connections
@@ -55,7 +57,7 @@ Horus.prototype.getUniqueConnections = function(connections) { // Get unique con
     return uniqueConnections;
     }
 
-    
+
 Horus.prototype.showConnections = function(connections) { // Show connections
     var popup = document.createElement('div');
     popup.id = 'horus-popup';
@@ -85,6 +87,101 @@ Horus.prototype.showConnections = function(connections) { // Show connections
     popup.appendChild(ul);
     document.body.appendChild(popup);
 
+    }
+
+Horus.prototype.detectCookies = function() { // Detect cookies
+    var self = this;
+    var cookies = document.cookie.split(';');
+    var uniqueCookies = this.getUniqueCookies(cookies);
+    //this.showCookies(uniqueCookies);
+    }
+
+Horus.prototype.getUniqueCookies = function(cookies) { // Get unique cookies
+    var uniqueCookies = cookies.map(function(cookie) {
+        return cookie.split('=')[0].trim();
+    });
+    var uniqueCookies = this.getUniqueConnections(uniqueCookies);
+    return uniqueCookies;
+    }
+
+Horus.prototype.showCookies = function(cookies) { // Show cookies
+    var popup = document.createElement('div');
+    popup.id = 'horus-popup';
+    popup.style.position = 'fixed';
+    popup.style.top = '0';
+    popup.style.right = '0';
+    popup.style.width = '300px';
+    popup.style.height = '100%';
+    popup.style.backgroundColor = 'white';
+    popup.style.zIndex = '999999';
+    popup.style.overflow = 'scroll';
+    popup.style.padding = '20px';
+    popup.style.boxShadow = '0 0 10px rgba(0, 0, 0, 0.5)';
+    popup.style.fontFamily = 'Arial';
+    popup.style.fontSize = '12px';
+    popup.style.color = 'black';
+    popup.style.textAlign = 'left';
+    popup.style.lineHeight = '1.5';
+    popup.style.boxSizing = 'border-box';
+    popup.innerHTML = '<h1>Cookies</h1>';
+    var ul = document.createElement('ul');
+    cookies.forEach(function(cookie) {
+        var li = document.createElement('li');
+        li.innerHTML = cookie;
+        ul.appendChild(li);
+    });
+    popup.appendChild(ul);
+    document.body.appendChild(popup);
+    }
+
+Horus.prototype.detectLocalStorage = function() { // Detect local storage
+    var self = this;
+    var localStorage = this.getLocalStorage();
+    var uniqueLocalStorage = this.getUniqueLocalStorage(localStorage);
+    this.showLocalStorage(uniqueLocalStorage);
+    }
+
+Horus.prototype.getLocalStorage = function() { // Get local storage
+    var localStorage = [];
+    for (var i = 0; i < localStorage.length; i++) {
+        localStorage.push(localStorage.key(i));
+    }
+    return localStorage;
+    }
+
+Horus.prototype.getUniqueLocalStorage = function(localStorage) { // Get unique local storage
+    var uniqueLocalStorage = this.getUniqueConnections(localStorage);
+    return uniqueLocalStorage;
+    }
+
+Horus.prototype.showLocalStorage = function(localStorage) { // Show local storage
+    var popup = document.createElement('div');
+    popup.id = 'horus-popup';
+    popup.style.position = 'fixed';
+    popup.style.top = '0';
+    popup.style.right = '0';
+    popup.style.width = '300px';
+    popup.style.height = '100%';
+    popup.style.backgroundColor = 'white';
+    popup.style.zIndex = '999999';
+    popup.style.overflow = 'scroll';
+    popup.style.padding = '20px';
+    popup.style.boxShadow = '0 0 10px rgba(0, 0, 0, 0.5)';
+    popup.style.fontFamily = 'Arial';
+    popup.style.fontSize = '12px';
+    popup.style.color = 'black';
+    popup.style.textAlign = 'left';
+    popup.style.lineHeight = '1.5';
+    popup.style.boxSizing = 'border-box';
+    popup.innerHTML = '<h1>Local Storage</h1>';
+    var ul = document.createElement('ul');
+    localStorage.forEach(function(item) {
+        var li = document.createElement('li');
+        li.innerHTML = item;
+        ul.appendChild(li);
+    });
+    popup.appendChild(ul);
+    document.body.appendChild(popup);
     }
 
 new Horus(); // Initialize Horus
